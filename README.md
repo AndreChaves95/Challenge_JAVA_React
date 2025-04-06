bd35a1eea4d24e063b35eec08c91a499350eefd1
Andre Chaves <andrefmoreirachaves@hotmail.com>
2025-04-06 16:26:11 +0100

    FT1: Create Product Entity

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-## JAVA and React Project
+# JAVA and React Project
+
+### Entity class
+#### Product: 
+- Id
+- Name
+- Price
+- Description
+
+#### Lombok
+- Use of Lombok library to automatically generate Getter, Setter, HashCode, ToString, etc methods
---
+++ src/main/java/com/natixis/project/entity/Product.java
@@ -0,0 +1,25 @@
+package com.natixis.project.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @NotNull
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private Double price;
+}

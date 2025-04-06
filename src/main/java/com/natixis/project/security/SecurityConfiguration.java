2b77a231afda8c36eeb419efa732618b70336225
Andre Chaves <andrefmoreirachaves@hotmail.com>
2025-04-06 17:59:56 +0100

    Small improve on Security

--- src/main/java/com/natixis/project/security/SecurityConfiguration.java
+++ src/main/java/com/natixis/project/security/SecurityConfiguration.java
@@ -27,7 +27,9 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                )
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }

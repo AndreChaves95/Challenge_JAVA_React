79a92aa3f6da3d8a6282de869444c216d383374c
Andre Chaves <andrefmoreirachaves@hotmail.com>
2025-04-08 23:36:30 +0100

    FT9: Add CORS and validation for invalid user credentials

--- product-catalog/src/pages/LoginPage.jsx
+++ product-catalog/src/pages/LoginPage.jsx
@@ -5,18 +5,25 @@ import { setAuth } from '../services/api';
 export default function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const handleUserLogin = (e) => {
     e.preventDefault();
-    setAuth(username, password);
-    navigate('/products');
+    if (username != 'admin' || password != 'admin123') {
+      setError('Invalid credentials!');
+      return;
+    } else {
+      setAuth(username, password);
+      navigate('/products');
+    }
   };
 
   return (
     <div className="container mt-5">
       <h2>Login into Products Catalog Project</h2>
       <form onSubmit={handleUserLogin}>
+        {error && <div className="alert alert-danger">{error}</div>}
         <input type="text" placeholder="Insert Username" className="form-control my-2"
                value={username} onChange={(e) => setUsername(e.target.value)} />
         <input type="password" placeholder="Insert Password" className="form-control my-2"
